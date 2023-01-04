0624bdad4f9ae7bce506dbf40f1afd3c23802c4a
Jeong Wang <jeong@jeongs-mbp.myfiosgateway.com>
2023-01-04 16:12:44 -0500

    first commit

---
+++ module.py
@@ -0,0 +1,11 @@
+from math import pi as p
+
+def printName(name):
+    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+
+
+def area(length, width):
+    return length*width
+
+def circumference(radius):
+    return 2*p*radius
